39f4caf0419163db3a203c50c57d2261e33c752b
Rivera Sebastian <sebas.rivmor@hotmail.com>
2024-05-20 16:45:52 -0500

    actualizado 20/05-16:45

--- main.py
+++ main.py
@@ -16,12 +16,31 @@ class MyWindow(QMainWindow):
         self.slot_window.show()
         self.close()  # Cerrar la ventana principal
 
+    def show_recargas(self):
+        self.slot_window = SlotWindow()
+        self.slot_window.show()
+        self.button_Recarga.clicked.connect(self.show_slot)
+
 class SlotWindow(QWidget):
 
     def __init__(self):
         super(SlotWindow, self).__init__()
         loadUi('C:\\Users\\usuario\\Desktop\\Proyect_Final\\Proyect_final\\Project\\slot.ui', self)
         self.setStyleSheet("background-image: url(C:\\Users\\usuario\\Desktop\\Proyect_Final\\Proyect_final\\img\\background.qrc.jpg)")
+        self.button_Recarga.clicked.connect(self.show_recargas)
+
+    def show_recargas(self):
+        self.recargas_window = RecargasWindow()
+        self.recargas_window.show()
+        self.close()  # Cierra la ventana actual
+
+class RecargasWindow(QWidget):
+
+    def __init__(self):
+        super(RecargasWindow, self).__init__()
+        loadUi('C:\\Users\\usuario\\Desktop\\Proyect_Final\\Proyect_final\\Project\\recargas.ui', self)
+        self.setStyleSheet("background-image: url(C:\\Users\\usuario\\Desktop\\Proyect_Final\\Proyect_final\\img\\background_2.png)")
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
