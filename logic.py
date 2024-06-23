4205d17c7901a8257bb1879e488085297f31039b
Rivera Sebastian <sebas.rivmor@hotmail.com>
2024-06-23 13:28:22 -0500

    Actualizado 23/06

--- logic.py
+++ logic.py
@@ -11,13 +11,13 @@ simbolo_contar = {
 }
 
 simbolo_valor = {
-    "symbol_apple": 2,       
-    "symbol_bar": 5,        
-    "symbol_bell": 8,      
-    "symbol_watermelon": 14,    
-    "symbol_limon": 17,     
-    "symbol_cherry": 22,    
-    "symbol_seven": 12      
+    "symbol_apple": 1000,       
+    "symbol_bar": 500,        
+    "symbol_bell": 100,      
+    "symbol_watermelon": 75,    
+    "symbol_limon": 50,     
+    "symbol_cherry": 25,    
+    "symbol_seven": 250      
 }
 
 def obtener_simbolo():
